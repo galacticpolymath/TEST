956f29fc218ece475c525c2877d99cc84c9623db
Matt Wilkins <matt@galacticpolymath.com>
2022-04-02 13:50:20 -0500

    galacticPubs::publish() [2022-04-02 13:50:19]

--- compile-lesson.R
+++ compile-lesson.R
@@ -1,7 +1,5 @@
 # Script for assembling a Galactic Polymath interdisciplinary lesson plan
 # ver 0.2.0
-
-
 remotes::install_github("galacticpolymath/galacticPubs")
 library(galacticPubs)
 googledrive::drive_auth() #need to authorize your account first time you use this
