dbb5e8c32e07ae28e8da54038d641b22aa257d04
Matt Wilkins <matt@galacticpolymath.com>
2022-04-12 18:38:16 -0500

    galacticPubs::publish() [2022-04-12 18:38:14]  updated graph

--- scripts/grafs.R
+++ scripts/grafs.R
@@ -10,17 +10,24 @@ calling$hr<-gsub("00h","",calling$Time_Block)
 g0 <- calling  %>% 
       ggplot(aes(y = Calls_per_hour, x = hr,col=Sex, shape=Sex)) + 
       geom_point(size=1.5) + 
-      theme_galactic(text.cex = c(1,1,1.2,1), axis.tick.length=3) + 
-      xlab("Time (hour)") + 
+      theme_galactic(text.cex=1.5,font.face = c(1,4,4)) + theme(legend.text = element_text(face=4,colour=gpColors("hydr")),
+                                                                axis.title=element_text(colour=gpColors("hydr"))) +
+      xlab("Time (hour of day)") + 
       ylab("Calls per Hour")+
-      ggtitle("Guardian Frog Call Rates by Sex")
+      ggtitle("Figure 1. Guardian Frog Call Rates by Sex")
 
 (g<-g0+scale_shape_manual(values=c(21,23))+
       scale_color_manual(values=c(gpPal[[2]]$hex[1],gpPal[[2]]$hex[13])) )
 gpsave("frog-call-rates_LABELED.png")
 
-g0+blank_labs(title_txt=NA)+scale_color_manual(values=c(gpPal[[2]]$hex[1],gpPal[[2]]$hex[13]),labels=c("____________","____________"))+scale_shape_manual(values=c(21,23),labels=c("____________","____________"))+theme(legend.key.size = unit(2, 'lines'),
+g0+blank_labs(title_txt=NA,font.face=1)+scale_color_manual(values=c(gpPal[[2]]$hex[1],gpPal[[2]]$hex[13]),labels=c("____________","____________"))+scale_shape_manual(values=c(21,23),labels=c("____________","____________"))+theme(legend.key.size = unit(2, 'lines'),
                                                                                                                                                                                                                          legend.key.width=unit(1,"line"))
 
 gpsave("frog-call-rates_BLANK.png") 
-                                                                                                                                                                                                                  
+                                                  
+#output data table for activity
+
+tbl<-rbind(head(calling,4),tail(calling,4,keepnums=T))[,-3]
+names(tbl)[3]<-"hour_of_day"
+tbl
+write.csv(tbl,"data/part2_data_table.csv")
